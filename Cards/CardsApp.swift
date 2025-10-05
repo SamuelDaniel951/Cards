74156284ec53f6488d46c4ccb6314cbd9e7b3848
Samuel Daniel <soma.hanna555@gmail.com>
2025-10-05 00:11:44 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Madiha Ibrahim on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
