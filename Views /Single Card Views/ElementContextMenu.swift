11d86475ebe5238f8ded95209c4da0c41509d2f2
Samuel Daniel <soma.hanna555@gmail.com>
2025-10-06 19:42:26 -0400

    ch18

---
+++ Views /Single Card Views/ElementContextMenu.swift
@@ -0,0 +1,18 @@
+//
+//  ElementContextMenu.swift
+//  Cards
+//
+//  Created by Madiha Ibrahim on 10/6/25.
+//
+
+import SwiftUI
+
+struct ElementContextMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ElementContextMenu()
+}
